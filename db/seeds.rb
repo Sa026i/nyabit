2c4e99f1e99f7813c61bdd20f24d573002fe114d
Saki Yamaguchi <Saki_Yamaguchii@yahoo.co.jp>
2025-11-09 20:58:40 +0900

    Seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+[
+    { code: "BC", name: "Black Cat" },
+    { code: "WC", name: "White Cat" },
+    { code: "OC", name: "Orange Cat" },
+    { code: "HC", name: "Hachiware Cat" },
+    { code: "SC", name: "Shamu Cat" },
+    { code: "GD", name: "Golden Dog" },
+    { code: "RP", name: "Red Panda" },
+].each do |cats|
+    partner = Partner.find_or_initialize_by(code: cats[:code])
+    partner.update!(cats)
+end
